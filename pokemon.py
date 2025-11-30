dc52ffc78bc3c1bd3894c427c17d8e2e947b608e
SwappawS <gregoires@seminaire-sherbrooke.qc.ca>
2025-11-30 20:02:39 +0000

    1.0

--- pokemon.py
+++ pokemon.py
@@ -2,15 +2,22 @@ class Pokemon:
     def __init__(self,name,nv,TauxSpawn,TauxCapture):
         self.name = name
         self.nv = nv
+        self.TauxSpawn = TauxSpawn
+        self.TauxCapture = TauxCapture
     def spawn(self):
         print("Un" + self.name + "de niveau" + self.nv + "sauvage apparait !")
 
-p1 = Pokemon("rattata","nvrat","TauxSpawnRat","TauxCaptureRat")
+p1 = Pokemon("rattata","nvrat","35","TauxCaptureRat")
+p2 = Pokemon("roucool","nvrouc","35","TauxCaptureBird")
+p3 = Pokemon("nidoran","nvnido","25","TauxCaptureLapin")
+p4 = Pokemon("mew","nvmew","5","TauxCaptureMew")
 
-p2 = Pokemon("magicarp","nvm","TauxSpawnFish","TauxCaptureFish")
+class Ball:
+    def __init__(self,name,nbrBallInv,BonusCapture):
+        self.name = name
+        self.nbrBallInv = nbrBallInv
+        self.BonusCapture = BonusCapture
 
-p3 = Pokemon("roucool","nvrouc","TauxSpawnBird","TauxCaptureBird")
-
-p4 = Pokemon("nidoran","nvnido","TauxSpawnLapin","TauxCaptureLapin")
-
-p5 = Pokemon("mew","nvmew","TauxSpawnMew","TauxCaptureMew")
+b1= Ball("pokeball","50","1")
+b2= Ball("superball","25","1.25")
+b3= Ball("hyperball","10","1.5")
