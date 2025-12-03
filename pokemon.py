f760152635230d897153c36b86b17698ca408ee0
SwappawS <gregoires@seminaire-sherbrooke.qc.ca>
2025-12-03 18:47:38 +0000

    1.100

--- pokemon.py
+++ pokemon.py
@@ -4,54 +4,48 @@ print("Bienvenue" + dresseur + ",est-tu prêt à tenter ta chance ?")
 
 
 class Pokemon:
-    def __init__(self,name,nv,TauxSpawn,TauxCapture):
+    def __init__(self, name, nv, TauxSpawn, TauxCapture):
         self.name = name
         self.nv = nv
         self.TauxSpawn = TauxSpawn
         self.TauxCapture = TauxCapture
-    def spawn(self):
-        print(" Un " + self.name + " de niveau " + self.nv + " sauvage apparait !")
 #code spawn 2 pokémon
 import random
 def generate_number():
-    rattata = 0
-    roucool = 1
-    nidoran = 2
-    mew = 3
     rand = random.random()
-    print(rand)
     if rand < 0.35:
-        return rattata
+        return 0
     elif rand < 0.70:  # 0.35 + 0.35
-        return roucool
+        return 1
     elif rand < 0.95:  # 0.70 + 0.25
-        return nidoran
+        return 2
     else:
-        return mew
-#code niveau
-if generate_number() == 0:
-    import random
-    NvPokemonSauvage = random.randint(2,10)
-elif generate_number() == 1:
-    import random
-    NvPokemonSauvage = random.randint(5,15)
-elif generate_number() == 2:
-    import random
-    NvPokemonSauvage = random.randint(8,15)
-elif generate_number() == 3:
-    import random
-    NvPokemonSauvage = random.randint(20,50)
+        return 3
 
-
-p1 = Pokemon("rattata","nvrat","35","TauxCaptureRat")
-p2 = Pokemon("roucool","nvrouc","35","TauxCaptureBird")
-p3 = Pokemon("nidoran","nvnido","25","TauxCaptureLapin")
-p4 = Pokemon("mew","nvmew","5","TauxCaptureMew")
+# Créer les templates de Pokémon (nv sera défini au spawn)
+p1 = Pokemon("rattata", None, 35, 80)
+p2 = Pokemon("roucool", None, 35, 75)
+p3 = Pokemon("nidoran", None, 25, 45)
+p4 = Pokemon("mew", None, 5, 10)
 
 pokemonList = [p1, p2, p3, p4]
+
+# Choisir l'espèce à spawn
 quelPOKEMON = generate_number()
 spawn1 = pokemonList[quelPOKEMON]
-print(" Un " + spawn1.name + " de niveau " + spawn1.nv + " sauvage apparait !")
+if quelPOKEMON == 0:
+    level = random.randint(2, 10)
+elif quelPOKEMON == 1:
+    level = random.randint(5, 15)
+elif quelPOKEMON == 2:
+    level = random.randint(8, 15)
+else:
+    level = random.randint(20, 50)
+
+# Affecter le niveau à l'instance spawnée
+spawn1.nv = level
+
+print("Un " + spawn1.name + " de niveau " + str(spawn1.nv) + " sauvage apparait !")
 
 TAUXCAPTURERAT=80
 TAUXCAPTUREBIRD=75
@@ -59,21 +53,25 @@ TAUXCAPTURELAPIN=45
 TAUXCAPTUREMEW=10
 
 class Ball:
-    def __init__(self,name,nbrBallInvMax,BonusCapture):
+    def __init__(self, name, nbrBallInvMax, BonusCapture):
         self.name = name
         self.nbrBallInvMax = nbrBallInvMax
         self.BonusCapture = BonusCapture
 
 
+b1 = Ball("pokeball", 50, 1)
+b2 = Ball( "superball", 25, 2)
+b3 = Ball("hyperball", 10, 3)
 
-b1= Ball("pokeball","50","1")
-b2= Ball("superball","25","2")
-b3= Ball("hyperball","10","3")
-
-QUESTION1="Que voulez-vous faire ? F=fuire C=Capturer"
-QUESTION2="Quelle pokeball voulez-vous utiliser ? P=pokeball, S=superball, H= hyperball"
+QUESTION1="Que voulez-vous faire ? F=fuire C=Capturer: "
+QUESTION2="Quelle pokeball voulez-vous utiliser ? P=pokeball, S=superball, H= hyperball: "
 
 action1=input(QUESTION1)
+if action1!="F" and action1!="C" and action1!="f" and action1!="c":
+    print("veuillez entrer une valeur correcte ! ")
+if action1=="F" or action1=="f":
+    print("Vous avez fui le pokemon sauvage ")
+    exit()
 
 typeball=input(QUESTION2)
 if typeball=="P":
@@ -82,25 +80,23 @@ if typeball=="S":
     Ball=b2
 if typeball=="H":
     Ball=b3
-elif typeball!="P" or "S" or "H":
-    print("veuillez entrer une valeur de pokeball possible")
+elif typeball!="P" or "S" or "H" or "F":
+    print("veuillez entrer une valeur de pokeball possible ")
 
-TauxCapture=Pokemon.TauxCapture
-NvPokemonSauvage=Pokemon.nv
+
+TauxCapture=spawn1.TauxCapture
+NvPokemonSauvage=spawn1.nv
 FacteurBall=int(Ball.BonusCapture)
 ChanceCapture=((TauxCapture * NvPokemonSauvage) * (FacteurBall))/100
 
 
-if action1=="F":
-    print("Vous avez fui le pokemon sauvage")
-if action1=="C":
+if action1=="C" or action1=="c":
     print("Vous lancer la pokeball au pokemon sauvage")
     import random
     ChanceDresseur=random.randint(1,100)
     if ChanceCapture<=ChanceDresseur:
-        print("Félicitation, vous avez capturé un" + Pokemon.name)
+        print("Félicitation, vous avez capturé un " + spawn1.name)
     else:
-        print("Dommage, le pokemon est sorti de la pokeball")
-        QUESTION4="Voulez-vous réessayer de capturer ou fuir ? C=Capturer F=Fuir"
-        repeat=action1=input(QUESTION4)
+        print("Dommage, le pokemon est sorti de la pokeball et a fui...")
+       
 
